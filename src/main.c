7c4682cd2c2d39719995909322cea0fd82620dba
Alexandre Sanson <alexandre.sanson@telecom-paris.fr>
2022-10-05 09:01:59 +0200

    Prise en main des threads : ajout d'une boucle infini dans un threads + changement de la priorité par défaut de shell_uart

--- src/main.c
+++ src/main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <zephyr/kernel.h>
+
+
+#define THREAD_STACK_SIZE 500
+#define PRIORITY 5
+
+void my_thread() {
+    printf("Hello, world\n");
+    while(1){}
+}
+
+K_THREAD_STACK_DEFINE(thread_stack_area, THREAD_STACK_SIZE);
+struct k_thread my_thread_data;
 
 int main() {
+
     printf("Hello, world\n");
+
+    k_tid_t tid = k_thread_create (&my_thread_data, thread_stack_area,
+                                      K_THREAD_STACK_SIZEOF(thread_stack_area),
+                                      my_thread,
+                                      NULL, NULL, NULL,
+                                      PRIORITY, 0, K_NO_WAIT);
+
+    k_thread_suspend(tid);
+    //k_thread_abort(tid);
+
     return 0;
 }
