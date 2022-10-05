9e83e1358f29449c04d762bfbf1c6b800df28278
Alexandre Sanson <alexandre.sanson@telecom-paris.fr>
2022-10-05 09:27:18 +0200

    GPIOA PIN5 toggle every seconds (using the device tree)

--- src/main.c
+++ src/main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/device.h>
 #include <zephyr/kernel.h>
 
 
-#define THREAD_STACK_SIZE 500
-#define PRIORITY 5
+#define THREAD_STACK_SIZE   500
+#define PRIORITY            5
+#define GPIOA_NODE          DT_NODELABEL(gpioa)
+
+// Get the struct device pointer to gpioa at compile time
+static const struct device * const gpioa_dev = DEVICE_DT_GET(GPIOA_NODE);
 
 void my_thread() {
     printf("Hello, world\n");
@@ -25,6 +31,17 @@ int main() {
 
     k_thread_suspend(tid);
     //k_thread_abort(tid);
+
+    if (!device_is_ready(gpioa_dev)) {
+        return -ENODEV;  // Device is not ready
+    }
+    // Configure the led as an output, initially inactive
+    gpio_pin_configure(gpioa_dev, 5, GPIO_OUTPUT_INACTIVE);
+    // Toggle the led every second
+    for (;;) {
+        gpio_pin_toggle(gpioa_dev, 5);
+        k_sleep(K_SECONDS(1));
+    }
 
     return 0;
 }
