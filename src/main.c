e9c2630c856804127e6bd11372d6279de3968266
Alexandre Sanson <alexandre.sanson@telecom-paris.fr>
2022-10-06 15:51:43 +0200

    Valeur de l'état de swp0 dans led0 sans utiliser de boucle infinie. On a utilisé une fonction callback. Aurait-il été possible de le faire avec IRQ_CONNECT ??

--- src/main.c
+++ src/main.c
@@ -1,52 +1,94 @@
-#include <stdio.h>
-#include <zephyr/drivers/gpio.h>
-#include <zephyr/drivers/led.h>
-#include <zephyr/device.h>
+/*
+ * Copyright (c) 2016 Open-RnD Sp. z o.o.
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/sys/util.h>
+#include <zephyr/sys/printk.h>
+#include <inttypes.h>
 
+#define SLEEP_TIME_MS	1
 
-#define THREAD_STACK_SIZE   500
-#define PRIORITY            5
-#define LEDS_NODE           DT_PATH(leds)
+/*
+ * Get button configuration from the devicetree sw0 alias. This is mandatory.
+ */
+#define SW0_NODE	DT_ALIAS(sw0)
 
-// Get the struct device pointer to leds at compile time
-const struct device * const leds_node = DEVICE_DT_GET(LEDS_NODE);
+/*
+ * Verify if the button swp0 exists
+ */
+#if !DT_NODE_HAS_STATUS(SW0_NODE, okay)
+#error "Unsupported board: sw0 devicetree alias is not defined"
+#endif
 
-void my_thread() {
-    printf("Hello, world\n");
-    while(1){}
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios, {0});
+static struct gpio_callback button_cb_data;
+
+/*
+ * The led0 devicetree alias is optional. If present, we'll use it
+ * to turn on the LED whenever the button is pressed.
+ */
+static struct gpio_dt_spec led = GPIO_DT_SPEC_GET_OR(DT_ALIAS(led0), gpios, {0});
+
+// Fonction executed when we press the button (raising and falling edge)
+void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+{
+	printk("Button pressed at %" PRIu32 "\n", k_cycle_get_32());
+	if (led.port) {
+		/* If we have an LED, match its state to the button's. */
+		int val = gpio_pin_get_dt(&button);
+		if (val >= 0) {
+			gpio_pin_set_dt(&led, val);
+		}
+		k_msleep(SLEEP_TIME_MS);
+	}
 }
 
-K_THREAD_STACK_DEFINE(thread_stack_area, THREAD_STACK_SIZE);
-struct k_thread my_thread_data;
+void main(void)
+{
+	int ret;
 
+	if (!device_is_ready(button.port)) {
+		printk("Error: button device %s is not ready\n", button.port->name);
+		return;
+	}
 
-int main() {
+	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+	if (ret != 0) {
+		printk("Error %d: failed to configure %s pin %d\n", ret, button.port->name, button.pin);
+		return;
+	}
 
-    printf("Hello, world\n");
+	// Interruption for swp0 on both raising and fallind edge
+	ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_BOTH );
+	if (ret != 0) {
+		printk("Error %d: failed to configure interrupt on %s pin %d\n", ret, button.port->name, button.pin);
+		return;
+	}
 
-    k_tid_t tid = k_thread_create (&my_thread_data, thread_stack_area,
-                                      K_THREAD_STACK_SIZEOF(thread_stack_area),
-                                      my_thread,
-                                      NULL, NULL, NULL,
-                                      PRIORITY, 0, K_NO_WAIT);
+	gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
+	gpio_add_callback(button.port, &button_cb_data);
+	printk("Set up button at %s pin %d\n", button.port->name, button.pin);
 
-    k_thread_suspend(tid);
-    //k_thread_abort(tid);
+	if (led.port && !device_is_ready(led.port)) {
+		printk("Error %d: LED device %s is not ready; ignoring it\n", ret, led.port->name);
+		led.port = NULL;
+	}
+	if (led.port) {
+		ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT);
+		if (ret != 0) {
+			printk("Error %d: failed to configure LED device %s pin %d\n", ret, led.port->name, led.pin);
+			led.port = NULL;
+		} else {
+			printk("Set up LED at %s pin %d\n", led.port->name, led.pin);
+		}
+	}
 
-    if (!device_is_ready(leds_node)) {
-        return -ENODEV;
-    }
-    for(;;) {
-        led_on(leds_node, 0);
-        led_on(leds_node, 1);
-        led_on(leds_node, 2);
-        k_sleep(K_SECONDS(1));
-        led_off(leds_node, 0);
-        led_off(leds_node, 1);
-        led_off(leds_node, 2);
-        k_sleep(K_SECONDS(1));
-    }
-
-        return 0;
+	printk("Press the button\n");
+	
 }
