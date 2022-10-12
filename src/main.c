6f9183402bd50fda697652e8c8e9b89b34c4e192
Alexandre Sanson <alexandre.sanson@telecom-paris.fr>
2022-10-12 10:53:01 +0200

    Using the VL5310x sensor, we print every half second the distance between the sensor and an obstacle above

--- src/main.c
+++ src/main.c
@@ -1,100 +1,45 @@
 /*
- * Copyright (c) 2016 Open-RnD Sp. z o.o.
- * Copyright (c) 2020 Nordic Semiconductor ASA
+ * Copyright (c) 2017 STMicroelectronics
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
-#include <zephyr/drivers/gpio.h>
-#include <zephyr/sys/util.h>
+#include <zephyr/drivers/sensor.h>
+#include <stdio.h>
 #include <zephyr/sys/printk.h>
-#include <inttypes.h>
-
-#define SLEEP_TIME_MS	1
-#define TIME_LED_ON_MS	1000
-
-/*
- * Get button configuration from the devicetree sw0 alias. This is mandatory.
- */
-#define SW0_NODE	DT_ALIAS(sw0)
-
-/*
- * Verify if the button swp0 exists
- */
-#if !DT_NODE_HAS_STATUS(SW0_NODE, okay)
-#error "Unsupported board: sw0 devicetree alias is not defined"
-#endif
-
-static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios, {0});
-static struct gpio_callback button_cb_data;
-
-/*
- * The led0 devicetree alias is optional. If present, we'll use it
- * to turn on the LED whenever the button is pressed.
- */
-static struct gpio_dt_spec led = GPIO_DT_SPEC_GET_OR(DT_ALIAS(led0), gpios, {0});
-
-// Work queue features
-//K_THREAD_STACK_DEFINE(work_stack_area, WORK_STACK_SIZE);
-struct k_work_delayable dwork;
-
-// Fonction executed when we press the button (raising edge)
-void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
-{
-	printk("I am in button_pressed\n");
-	gpio_pin_set_dt(&led, 1); // LED ON
-	k_work_reschedule(&dwork, K_MSEC(TIME_LED_ON_MS)); // Enter working item aftr 1 second
-}
-
-void working_led_behavior() {
-	printk("I am in led_behavior\n\n");
-	gpio_pin_set_dt(&led, 0); // LED OFF	
-}
 
 void main(void)
 {
+	const struct device *const dev = DEVICE_DT_GET_ONE(st_vl53l0x);
+	struct sensor_value value;
 	int ret;
 
-	k_work_init_delayable(&dwork, working_led_behavior);
-
-	if (!device_is_ready(button.port)) {
-		printk("Error: button device %s is not ready\n", button.port->name);
+	if (dev == NULL) {
+		/* No such node, or the node does not have status "okay". */
+		printk("\nError: no device %s found.\n", dev->name);
 		return;
 	}
 
-	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
-	if (ret != 0) {
-		printk("Error %d: failed to configure %s pin %d\n", ret, button.port->name, button.pin);
+	if (!device_is_ready(dev)) {
+		printk("sensor : device %s not ready.\n", dev->name);
 		return;
 	}
 
-	// Interruption for swp0 on both raising edge
-	ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_FALLING );
-	if (ret != 0) {
-		printk("Error %d: failed to configure interrupt on %s pin %d\n", ret, button.port->name, button.pin);
-		return;
-	}
+	printk ("Found device %s, getting sensor data\n", dev->name);
 
-	gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
-	gpio_add_callback(button.port, &button_cb_data);
-	printk("Set up button at %s pin %d\n", button.port->name, button.pin);
+	while (1) {
 
-	if (led.port && !device_is_ready(led.port)) {
-		printk("Error %d: LED device %s is not ready; ignoring it\n", ret, led.port->name);
-		led.port = NULL;
-	}
-	if (led.port) {
-		ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT);
-		if (ret != 0) {
-			printk("Error %d: failed to configure LED device %s pin %d\n", ret, led.port->name, led.pin);
-			led.port = NULL;
-		} else {
-			printk("Set up LED at %s pin %d\n", led.port->name, led.pin);
+		ret = sensor_sample_fetch(dev);
+		if (ret) {
+			printk("sensor_sample_fetch failed ret %d\n", ret);
+			return;
 		}
+
+		sensor_channel_get(dev, SENSOR_CHAN_DISTANCE, &value);
+		printf("distance is %.3fm\n", sensor_value_to_double(&value));
+
+		k_sleep(K_MSEC(500));
 	}
-
-	printk("Press the button\n");
-
 }
