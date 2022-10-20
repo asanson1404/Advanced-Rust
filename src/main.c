92c1250728aefa99db7a56116ee4be0a65ad4640
Alexandre Sanson <alexandre.sanson@telecom-paris.fr>
2022-10-20 19:52:29 +0200

    On détermine l'angle d'inclinaison des 3 axes en utilisant seulement l'accéléromètre. Les valeurs sont très satisfaisantes !

--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/i2c.h>
 #include <stdio.h>
+#include <math.h>
 #include <zephyr/sys/printk.h>
 
 #define LSM6DSL_NODE DT_NODELABEL(lsm6dsl)
@@ -32,16 +33,33 @@ void interrupt_handler(const struct device *dev, struct gpio_callback *cb, uint3
 }
 
 void read_value(struct k_work *item) {
+	
 	uint8_t addr_reg1 = 0x28;
 	int16_t data[3];
+	float acc_tot;
+
 	i2c_write_read_dt(&lsm6dsl, &addr_reg1, 1, &data, 6);
-	printk("X = %d, Y = %d, Z = %d\n", data[0], data[1], data[2]);
+	//printk("X = %d, Y = %d, Z = %d\n", data[0], data[1], data[2]);
+
+	// valeur totale de l'accélération, peu importe l'orientation
+	acc_tot = sqrt(data[0]*data[0] + data[1]*data[1] + data[2]*data[2]);
+
+	// calcul des angles et conversion en degrés
+	float angleX = asinf(data[0]/acc_tot) * 180.0/3.14158;
+	float angleY = asinf(data[1]/acc_tot) * 180.0/3.14158;
+	float angleZ = acosf(data[2]/acc_tot) * 180.0/3.14158;
+
+	printk("ANGLE_X = %.2f, ANGLE_Y = %.2f, ANGLE_Z = %.2f\n", angleX, angleY, angleZ);
+
 }
 
 
 void main(void)
 {
 	int ret;
+
+	float test = cos(0.0);
+	printk("TEST DU COSINUS  cos(0) = %f", test);
 
 	k_work_init(&my_work_q, read_value);
 
@@ -64,9 +82,13 @@ void main(void)
 	i2c_reg_write_byte_dt(&lsm6dsl, 0x15, (uint8_t) (1 << 4));
 	// CTRL1_XL ==> the sensor performs measures at 1.6Hz 
 	i2c_reg_write_byte_dt(&lsm6dsl, 0x10, (uint8_t) (0b1011 << 4));
-	// Configure register to raise an interrupt on INT1 pad when Accelerometer Data Ready
-	i2c_reg_write_byte_dt(&lsm6dsl, 0x0d, (uint8_t) 0x1);
-	
+	// Configure register to raise an interrupt on INT1 pad when Accelerometer Data Ready & Gyroscope Data Ready
+	i2c_reg_write_byte_dt(&lsm6dsl, 0x0d, (uint8_t) 0x3);
+
+	// X offset accelerometer
+	i2c_reg_write_byte_dt(&lsm6dsl, 0x73, (int) 1);
+	// Y offset accelerometer
+	i2c_reg_write_byte_dt(&lsm6dsl, 0x74, (int) 18);
 
 	//============================================================
 		// Config interrupt
