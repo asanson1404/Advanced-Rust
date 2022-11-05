b906f84b5eb73914d33d2bec72443e370cd75019
Alexandre Sanson <alexandre.sanson@telecom-paris.fr>
2022-11-05 18:05:13 +0100

    On a rajouté : led0 et led1 + un nouveau thread qui les fait clignotter + une message queue qui envoie les angles X et Y calculés au thread

--- TP1/src/main.c
+++ TP1/src/main.c
@@ -12,12 +12,29 @@
 #include <math.h>
 #include <zephyr/sys/printk.h>
 
-#define LSM6DSL_NODE DT_NODELABEL(lsm6dsl)
-#define PI 3.14158
-#define GYRO_SPEED 1.0/104.0
+
+#define THREAD_STACK_SIZE   512
+#define PRIORITY            5
+#define LSM6DSL_NODE 		DT_NODELABEL(lsm6dsl)
+#define PI 					3.14158
+#define GYRO_SPEED 			1.0/104.0
 
 static const struct i2c_dt_spec  lsm6dsl     = I2C_DT_SPEC_GET(LSM6DSL_NODE);
 static const struct gpio_dt_spec irq_lsm6dsl = GPIO_DT_SPEC_GET(LSM6DSL_NODE, irq_gpios);
+static struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET_OR(DT_ALIAS(led0), gpios, {0});
+static struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET_OR(DT_ALIAS(led1), gpios, {0});
+
+// Define the thread stack area
+K_THREAD_STACK_DEFINE(leds_thread_stack_area, THREAD_STACK_SIZE);
+struct k_thread my_thread_data;
+
+// Preparing the messages queue
+struct msg_queue_type {
+	float tilt_x;
+	float tilt_y;
+};
+char __aligned(4) msgq_buffer[sizeof(struct msg_queue_type)];
+struct k_msgq msgq;
 
 static struct gpio_callback lsm6dsl_cb_data;
 struct k_work   my_work_q;
@@ -48,7 +65,7 @@ static void interrupt_handler(const struct device *dev, struct gpio_callback *cb
 }
 
 static void read_value(struct k_work *item) {
-	
+
 	uint8_t first_reg_addr = 0x1e;
 
 	struct {
@@ -113,14 +130,41 @@ static void compute_and_print(struct k_timer *timer)
 	float angleX_filter = 0.95 * angleX_xl + 0.05 * angleX_g;
 	float angleY_filter = 0.95 * angleY_xl + 0.05 * angleY_g;
 
+	struct msg_queue_type buf = {angleX_filter, angleY_filter};
+
 	// Angle X pour les 3 méthode de calcul "ACC GYR FILTRE"
-	printk("%.2f   %.2f   %.2f\n", angleX_xl, angleX_g, angleX_filter);
+	//printk("%.2f   %.2f   %.2f\n", angleX_xl, angleX_g, angleX_filter);
 
 	// Angle Y pour les 3 méthode de calcul "ACC GYR FILTRE"
 	//printk("%.2f   %.2f   %.2f\n", angleY_xl, angleY_g, angleY_filter);
 
+	// send data to the thread
+	while (k_msgq_put(&msgq, &buf, K_NO_WAIT) != 0) {
+       	// message queue is full: purge old data & try again
+       	k_msgq_purge(&msgq);
+	}
+
 }
 
+// Thread to let the leds blink according to the tilt
+// One led for X axis and one led for Y axis
+// When the board is flat, leds are steady
+// Leds blinks very fast when the tilt angle is 90°
+void leds_thread() {
+	struct msg_queue_type tilt_data;
+	float tilt_x, tilt_y;
+	gpio_pin_set_dt(&led0, 1);
+	gpio_pin_set_dt(&led1, 1);
+    while(1){
+		k_msgq_get(&msgq, &tilt_data, K_NO_WAIT);
+		tilt_x = tilt_data.tilt_x;
+		tilt_y = tilt_data.tilt_y;
+		printk("%.2f   %.2f\n", tilt_x, tilt_y);
+		gpio_pin_toggle_dt(&led0);
+		gpio_pin_toggle_dt(&led1);
+        k_sleep(K_SECONDS(1));
+	}
+}
 
 void main(void)
 {
@@ -178,13 +222,56 @@ void main(void)
 	gpio_init_callback(&lsm6dsl_cb_data, interrupt_handler, BIT(irq_lsm6dsl.pin));
 	gpio_add_callback(irq_lsm6dsl.port, &lsm6dsl_cb_data);
 
+	//============================================================
+		// Config leds
+	//============================================================
+	//+++++++++++++++ LED0 ++++++++++++++++++++++
+	if (led0.port && !device_is_ready(led0.port)) {
+		printk("Error : LED device %s is not ready; ignoring it\n", led0.port->name);
+		led0.port = NULL;
+	}
+	if (led0.port) {
+		ret = gpio_pin_configure_dt(&led0, GPIO_OUTPUT);
+		if (ret != 0) {
+			printk("Error %d: failed to configure LED device %s pin %d\n", ret, led0.port->name, led0.pin);
+			led0.port = NULL;
+		} else {
+			printk("Set up LED at %s pin %d\n\n", led0.port->name, led0.pin);
+		}
+	}
+	//+++++++++++++++ LED1 ++++++++++++++++++++++
+	if (led1.port && !device_is_ready(led1.port)) {
+		printk("Error : LED device %s is not ready; ignoring it\n", led1.port->name);
+		led1.port = NULL;
+	}
+	if (led1.port) {
+		ret = gpio_pin_configure_dt(&led1, GPIO_OUTPUT);
+		if (ret != 0) {
+			printk("Error %d: failed to configure LED device %s pin %d\n", ret, led1.port->name, led1.pin);
+			led1.port = NULL;
+		} else {
+			printk("Set up LED at %s pin %d\n\n", led1.port->name, led1.pin);
+		}
+	}
 
 	printk("All devices are ready.\nStarting ....... \n\n\n");
 
 	read_who_am_i();
+
+	// Initializing the message queue
+	k_msgq_init(&msgq, msgq_buffer, sizeof(struct msg_queue_type), 1);
+
+	// Creating thread to manage the leds blinking
+	k_thread_create (&my_thread_data, leds_thread_stack_area,
+                     K_THREAD_STACK_SIZEOF(leds_thread_stack_area),
+                     leds_thread,
+                     NULL, NULL, NULL,
+                     PRIORITY, 0, K_NO_WAIT);
+
 	// Necessary to enter a first time in the interrupt
 	read_value(&my_work_q);
 
 	// We print values every 10ms (100Hz)
 	k_timer_start(&print_timer, K_SECONDS(1), K_MSEC(10));
+
 }
