793333c91a2fd72650b6ff8e50cdf8e14b77e9a6
Alexandre Sanson <alexandre.sanson@telecom-paris.fr>
2022-11-30 12:23:27 +0100

    Bon avancement partie 2.3, fin de la Q5

--- pwdchk/src/account.rs
+++ pwdchk/src/account.rs
@@ -14,19 +14,17 @@ impl Account {
     Account {login: login.to_string(), password: password.to_string()}
   }
 
-  pub fn from_string(s: &str) -> Self {
-    let v: Vec<&str> = s.splitn(2, ':').collect();
-    Account {login: v[0].to_string(), password: v[1].to_string()}
-  }
-
 }
 
 impl FromStr for Account {
     type Err = NoColon;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
+      if s.contains(':') {
         let v: Vec<&str> = s.splitn(2, ':').collect();
-        Ok(Account {login: v[0].to_string(), password: v[1].to_string()})
+        return Ok(Account {login: v[0].to_string(), password: v[1].to_string()});
       }
+      Err(NoColon)
+    }
 }
   
--- pwdchk/src/main.rs
+++ pwdchk/src/main.rs
@@ -1,21 +1,29 @@
 mod account;
-pub use account::Account;
+pub use account::*;
 use std::str::FromStr;
+use std::env;
 
-fn main() {
+fn main() -> Result<(), NoColon> {
 
     // PARTIE 2.1
     //let account = Account::new("johndoe", "super:complex:password");
     //println!("{:#?}", account);
 
     // PARTIE 2.2
-    println!("{:#?}", Account::from_string("johndoe:super:complex:password"));
+    //println!("{:#?}", Account::from_string("johndoe:super:complex:password"));
 
     // PARTIE 2.3
-    match Account::from_str("johndoe") {
-        Ok(account) => println!("{account:?}"),
-        Err(e) => println!("Erreur {e:?}"),
-      }
+    //match Account::from_str("johndoe:azerty") {
+    //    Ok(account) => println!("{account:?}"),
+    //    Err(e) => println!("Erreur {e:?}"),
+    //  }
+    //println!("{:#?}", Account::from_str("johndoe")?);
+    //Ok(())
+
+    let v: Vec<Result<_, _>> = env::args().map(|c| Account::from_str(c.as_str())).collect();
+    let accounts = &v[1..];
+    println!("{accounts:#?}");
+    Ok(())
     
-  }
+}
   
