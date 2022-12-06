fe7505f4d034cf9ec445e248113bca38205cc5a2
Alexandre Sanson <alexandre.sanson@telecom-paris.fr>
2022-12-06 15:14:18 +0100

    Fin de la partie 2.6, liste de compte chargée depuis un fichier, bonne gestion des erreurs

--- pwdchk/src/account.rs
+++ pwdchk/src/account.rs
@@ -2,6 +2,11 @@ use std::str::FromStr;
 use std::collections::HashMap;
 use std::vec;
 use std::fmt::Display;
+use crate::error;
+use std::path::Path;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::BufRead;
 use std::error::Error;
 
 #[derive(Debug)]
@@ -9,8 +14,6 @@ pub struct Account {
     login:    String, 
     password:  String,
 }
-#[derive(Debug)]
-pub struct NoColon;
 
 impl Account {
 
@@ -28,29 +31,46 @@ impl Account {
     }
     map
   }
+
+  pub fn from_file(filename: &Path) -> Result<Vec<Account>, error::Error> {
+
+    let f = File::open(filename).map_err(error::Error::from);
+    let reader = BufReader::new(f?);
+
+    let mut accounts = Vec::<Account>::new();
   
+
+    for line in reader.lines() {
+      accounts.push(Account::from_str(&line.unwrap())?);
+    }
+
+    Ok(accounts)
+  }
 
 }
 
 impl FromStr for Account {
-    type Err = NoColon;
+    type Err = error::Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
       if s.contains(':') {
         let v: Vec<&str> = s.splitn(2, ':').collect();
         return Ok(Account {login: v[0].to_string(), password: v[1].to_string()});
       }
-      Err(NoColon)
+      Err(error::Error::NoColon)
     }
 }
 
-impl Display for NoColon {
+impl Display for error::Error {
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
     write!(f, "Error message")
   }
 }
 
-impl Error for NoColon {
-
-}
+impl Error for error::Error {}
   
+impl From<std::io::Error> for error::Error {
+  fn from(item: std::io::Error) -> Self {
+    error::Error::IoError(item)
+  }
+}
--- pwdchk/src/main.rs
+++ pwdchk/src/main.rs
@@ -1,6 +1,8 @@
-use clap::{Args, Parser, Subcommand};
+use clap::{Args, Parser, Subcommand, ArgGroup};
 mod account;
 use account::*;
+use std::path::PathBuf;
+mod error;
 
 #[derive(Parser)]
 #[clap(version, author, about)]
@@ -16,26 +18,47 @@ enum Command {
 }
 
 #[derive(Args)]
+#[clap(group(
+    ArgGroup::new("input")
+        .required(true)
+        .args(&["account", "file"]),
+))]
 struct GroupArgs {
-    #[clap(required = true)]
+    #[clap(required = false)]
     /// Account to check
     account: Vec<Account>,
+    #[clap(short, long)]
+    /// Load passwords from a file
+    file: Option<PathBuf>,
 }
 
-fn main() {
+fn main() -> Result<(), error::Error> {
     let args = AppArgs::parse();
     match args.command {
         Command::Group(args) => {   // args is of type GroupArgs here
 
-           // Grouper les comptes collectés dans args.account, les filtrer, afficher
-           // ceux ayant plus d'un login en réutilisant le code écrit précédemment.
-           let mut my_hash_map = Account::group(args.account); 
-            my_hash_map.retain(|_, v| v.len() > 1);
+            match args.file {
 
-            for same_pwd_account in my_hash_map {
-                println!("Password {} used by {}", same_pwd_account.0, same_pwd_account.1.join(", "));
-            } 
+                Some(f) => {
+                    let accounts_ff = Account::from_file(&f)?;
+                    for account in accounts_ff {
+                        println!("{:#?}", account);
+                    }
+                },
+
+                None             => {
+                    // Grouper les comptes collectés dans args.account, les filtrer, afficher
+                    // ceux ayant plus d'un login en réutilisant le code écrit précédemment.
+                    let mut my_hash_map = Account::group(args.account); 
+                    my_hash_map.retain(|_, v| v.len() > 1);
+
+                    for same_pwd_account in my_hash_map {
+                        println!("Password {} used by {}", same_pwd_account.0, same_pwd_account.1.join(", "));
+                    }
+                }
+            }
         }
     }
+    Ok(())
 }
   
